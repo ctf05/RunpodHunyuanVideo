aa4dfc4153577c295dda32c7402cad935e9fb548
Christian Beadling <christianbeadling@gmail.com>
2024-12-08 20:58:11 -0500

    test remove sage for low vram errors

--- handler.py
+++ handler.py
@@ -135,7 +135,7 @@ def handler(job):
             return {"error": "Missing required parameter: prompt"}
 
         # Extract parameters
-        prompt = job_input["prompt", "high quality nature video of a red panda balancing on a bamboo stick while a bird lands on the panda's head, there's a waterfall in the background"]
+        prompt = job_input.get("prompt", "high quality nature video of a red panda balancing on a bamboo stick while a bird lands on the panda's head, there's a waterfall in the background")
         negative_prompt = job_input.get("negative_prompt", "bad quality video")
         width = job_input.get("width", 512)
         height = job_input.get("height", 288)
